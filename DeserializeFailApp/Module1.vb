75121156c2a572f0ad391aa486ebd972c0c6b4a7
Shi Hao Wang <st890409@hotmail.com>
2017-11-30 16:07:31 +0800

    在 VB.NET 進行 Json 字串反序列成物件錯誤範例

---
+++ DeserializeFailApp/Module1.vb
@@ -0,0 +1,27 @@
+﻿Imports Newtonsoft.Json
+
+Module Module1
+
+    Sub Main()
+        Dim _Json As String
+        Dim _User As User
+
+        _Json = <s>{"No":1,"Name":"txstudio"}</s>
+        _User = JsonConvert.DeserializeObject(_Json)
+
+        Console.WriteLine()
+        Console.WriteLine("No   : {0}", _User.No)
+        Console.WriteLine("Name : {0}", _User.Name)
+        Console.WriteLine()
+
+        Console.WriteLine("press any key to exit")
+        Console.ReadKey()
+    End Sub
+
+
+    Public Class User
+        Public Property No As Integer
+        Public Property Name As String
+    End Class
+
+End Module
